41845553f583c73f48d2fa34252a1bb3234c8bbc
wangxuehui <skyim@lotuseed.com>
2014-12-06 10:40:36 +0800

    delete the previous packages

--- src/main/java/in/srid/serializer/Deserializer.java
+++
@@ -1,5 +0,0 @@
-package in.srid.serializer;
-
-public interface Deserializer {
-    <T> T deserialize( final byte[] bytes , final Class<T> clazz );
-}
--- src/main/java/in/srid/serializer/Serializer.java
+++
@@ -1,5 +0,0 @@
-package in.srid.serializer;
-
-public interface Serializer {
-    <T> byte[] serialize( T source );
-}
